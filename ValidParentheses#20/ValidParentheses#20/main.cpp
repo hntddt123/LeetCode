04c71712ee2e736f19f0bf2cd77d0040980cfd9e
Nientai Ho <hntddt123@gmail.com>
2018-07-27 12:49:58 -0500

    Rearrange fileNames

---
+++ ValidParentheses#20/ValidParentheses#20/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  ValidParentheses#20
+//
+//  Created by Nientai Ho on 2018/7/27.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+
+#include <iostream>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
