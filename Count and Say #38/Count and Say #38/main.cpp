7d5af2dba4d8bea3685307d35fa2dfade48766aa
Nientai Ho <hntddt123@gmail.com>
2018-07-29 12:12:33 -0500

    Add #38 Folders to LeetCode

---
+++ Count and Say #38/Count and Say #38/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Count and Say #38
+//
+//  Created by Nientai Ho on 2018/7/29.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
