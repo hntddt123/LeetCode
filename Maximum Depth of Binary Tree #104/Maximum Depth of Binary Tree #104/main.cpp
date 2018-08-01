151a9e2b9f00a43206a8080c556d95d06e2a7d25
Nientai Ho <hntddt123@gmail.com>
2018-07-31 23:36:06 -0500

    Done #104

--- Maximum Depth of Binary Tree #104/Maximum Depth of Binary Tree #104/main.cpp
+++ Maximum Depth of Binary Tree #104/Maximum Depth of Binary Tree #104/main.cpp
@@ -6,17 +6,12 @@
 //  Copyright © 2018年 HNTStudio. All rights reserved.
 //
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-
 // EX: Given binary tree [3,9,20,null,null,15,7]
+//1       3[0]
+//     /        \
+//2  9[1] 2n+1 20[2] 2n+2
+//   /  \      /  \
+//3 [3] [4] 15[5] 7[6]
 // return depth = 3.
 
 #include <iostream>
@@ -32,15 +27,25 @@ struct TreeNode {
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        int depth = 0;
-        
-        return depth;
+        if (root == nullptr)
+            return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
 int main(int argc, const char * argv[]) {
     Solution s;
-    TreeNode root(10);
-    s.maxDepth(&root);
+    TreeNode root(3);
+    TreeNode node1(9);
+    TreeNode node2(20);
+    TreeNode node5(15);
+    TreeNode node6(7);
+
+    root.left = &node1;
+    root.right = &node2;
+    node2.left = &node5;
+    node2.right = &node6;
+
+    cout << "Tree Depth: " << s.maxDepth(&root) << endl;
     
     
     
