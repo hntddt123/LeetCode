cc5419e91c6f9309d27d154bbfa6b4f513616915
Nientai Ho <hntddt123@gmail.com>
2018-08-01 11:12:28 -0500

    Done #283

--- Move Zeroes #283/Move Zeroes #283/main.cpp
+++ Move Zeroes #283/Move Zeroes #283/main.cpp
@@ -5,11 +5,51 @@
 //  Created by Nientai Ho on 2018/8/1.
 //  Copyright © 2018年 HNTStudio. All rights reserved.
 //
+//  Input:  [0,1,0,3,12]
+//  Output: [1,3,12,0,0]
+
 
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int nonZeroCount = 0;
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroCount] = nums[i];
+                nonZeroCount++;
+            }
+        }
+        for (int i=nonZeroCount; i<nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
 
 int main(int argc, const char * argv[]) {
+    vector<int> nums;
+    nums.push_back(0);
+    nums.push_back(1);
+    nums.push_back(0);
+    nums.push_back(3);
+    nums.push_back(12);
+
+    Solution s;
     
+    for (int i=0; i<nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    s.moveZeroes(nums);
+    
+    for (int i=0; i<nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
     
     return 0;
 }
