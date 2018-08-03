07d6998312bbdea7cdc00be9aeb2dc2a9e9a9bd5
Nientai Ho <hntddt123@gmail.com>
2018-08-02 19:06:37 -0500

    Start #206

---
+++ Reverse Linked List #206/Reverse Linked List #206/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  Reverse Linked List #206
+//
+//  Created by Nientai Ho on 2018/8/2.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+//  Input: 1->2->3->4->5->NULL
+//  Output: 5->4->3->2->1->NULL
+
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+    }
+};
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
