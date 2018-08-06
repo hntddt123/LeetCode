0614daeccd4607985e24a77019acb79713b321d7
Nientai Ho <hntddt123@gmail.com>
2018-08-06 15:24:33 -0500

    Start #122

---
+++ Best Time to Buy and Sell Stock II #122/Best Time to Buy and Sell Stock II #122/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Best Time to Buy and Sell Stock II #122
+//
+//  Created by Nientai Ho on 2018/8/6.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0;
+        
+        return max;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution s;
+    vector<int> prices;
+    int result = s.maxProfit(prices);
+    cout << result << endl;
+    
+    return 0;
+}
