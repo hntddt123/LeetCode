dbf33e9cbc6c45643786311c7ad250c8fc782d01
Nientai Ho <hntddt123@gmail.com>
2018-08-05 21:57:49 -0500

    Done #171

--- Excel Sheet Column Number #171/Excel Sheet Column Number #171/main.cpp
+++ Excel Sheet Column Number #171/Excel Sheet Column Number #171/main.cpp
@@ -4,16 +4,28 @@
 //
 //  Created by Nientai Ho on 2018/8/5.
 //  Copyright © 2018年 HNTStudio. All rights reserved.
-//
+//  26 26 26
+//  A -> 1
+//  B -> 2
+//  C -> 3
+//  ...
+//  Z -> 26
+//  AA -> 27
+//  AB -> 28
+//  AC -> 29
+//  AAA -> 703
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Solution {
 public:
     int titleToNumber(string s) {
         int number = 0;
-        
+        for (int i = 0; i < s.size(); i++) {
+            number = (number * 26) + (s[i] - 'A' + 1);
+        }
         return number;
     }
 };
@@ -22,7 +34,8 @@ int main(int argc, const char * argv[]) {
     
     Solution s;
     string title = "AB";
-    s.titleToNumber(title)
+    int result = s.titleToNumber(title);
+    cout << result << endl;
     
     return 0;
 }
