589776cb5044d1dbd9b87c672e4126eb6628eeda
Nientai Ho <hntddt123@gmail.com>
2018-08-05 21:19:29 -0500

    Start #171

---
+++ Excel Sheet Column Number #171/Excel Sheet Column Number #171/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  Excel Sheet Column Number #171
+//
+//  Created by Nientai Ho on 2018/8/5.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int number = 0;
+        
+        return number;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    
+    Solution s;
+    string title = "AB";
+    s.titleToNumber(title)
+    
+    return 0;
+}
