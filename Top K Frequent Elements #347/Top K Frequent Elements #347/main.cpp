8cc2f1196783bbbac257875e97ef41ca7355be21
Nientai Ho <hntddt123@gmail.com>
2018-08-07 19:35:31 -0500

    Done #347

--- Top K Frequent Elements #347/Top K Frequent Elements #347/main.cpp
+++ Top K Frequent Elements #347/Top K Frequent Elements #347/main.cpp
@@ -8,14 +8,35 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int> topNumbers;
+        map<int, int> frequencyTable;
+        multimap<int, int> sortByValueMap;
+        vector<int> frequency;
+        if (nums.size() == 1) {
+            return nums;
+        }
+        for (int i=0; i<nums.size(); i++) {
+            frequencyTable[nums[i]]++ ;
+        }
+        for (auto i : frequencyTable) {
+            sortByValueMap.insert(make_pair(i.second, i.first));
+        }
+        for (auto i = sortByValueMap.rbegin(); i != sortByValueMap.rend(); i++) {
+            cout << "[" << i->second << ", " << i->first << " times] ";
+            frequency.push_back(i->second);
+        }
         
-        return topNumbers;
+        for (int i=0; i<frequency.size(); i++) {
+            frequency.erase(frequency.begin() + k, frequency.end());
+        }
+        
+        cout << endl;
+        return frequency;
     }
 };
 
@@ -23,17 +44,22 @@ int main(int argc, const char * argv[]) {
     Solution s;
     vector<int>topFrequentNumbers;
     int k = 2;
+    topFrequentNumbers.push_back(4);
     topFrequentNumbers.push_back(1);
-    topFrequentNumbers.push_back(1);
-    topFrequentNumbers.push_back(1);
+    topFrequentNumbers.push_back(-1);
     topFrequentNumbers.push_back(2);
+    topFrequentNumbers.push_back(-1);
     topFrequentNumbers.push_back(2);
     topFrequentNumbers.push_back(3);
     topFrequentNumbers = s.topKFrequent(topFrequentNumbers,k);
+    cout << "Result: [";
     for (int i=0; i<topFrequentNumbers.size(); i++) {
-        cout << topFrequentNumbers[i] << " ";
+        cout  << topFrequentNumbers[i];
+        if (i<topFrequentNumbers.size()-1) {
+            cout << ",";
+        }
     }
-    cout << endl;
+    cout << "]" << endl;
     
     return 0;
 }
