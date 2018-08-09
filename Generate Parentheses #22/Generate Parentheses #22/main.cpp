31797ebd10e7e82c0c33be80c01b93396e185456
Nientai Ho <hntddt123@gmail.com>
2018-08-08 21:12:29 -0500

    Start #22

---
+++ Generate Parentheses #22/Generate Parentheses #22/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Generate Parentheses #22
+//
+//  Created by Nientai Ho on 2018/8/8.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
