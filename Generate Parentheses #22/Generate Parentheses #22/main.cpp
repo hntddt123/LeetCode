6830718ed7e901a003bc472afc1addb1e6647af1
Nientai Ho <hntddt123@gmail.com>
2018-08-09 18:06:45 -0500

    Done #22

--- Generate Parentheses #22/Generate Parentheses #22/main.cpp
+++ Generate Parentheses #22/Generate Parentheses #22/main.cpp
@@ -5,14 +5,50 @@
 //  Created by Nientai Ho on 2018/8/8.
 //  Copyright © 2018年 HNTStudio. All rights reserved.
 //
-// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+//  Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+//  n = 3
+//  [ "((()))", "(()())", "(())()", "()(())", "()()()" ]
+//  n = 2
+//  [ "(())", "()()" ]
+//  n = 1
+//  [ "()" ]
+//  n = 0
+//  [ ]
 
 #include <iostream>
+#include <vector>
+#include <stack>
 using namespace std;
 
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> parentheses;
+        adddingParenthesis(parentheses, "", n, 0);
+        return parentheses;
+    }
+    void adddingParenthesis(vector<string> &parentheses, string str, int openParenthesisCount, int closeParenthesisCount) {
+        if (openParenthesisCount == 0 && closeParenthesisCount == 0) {
+            parentheses.push_back(str);
+        } else {
+            if (openParenthesisCount > 0) {
+                adddingParenthesis(parentheses, str + "(", openParenthesisCount-1, closeParenthesisCount+1);
+            }
+            if (closeParenthesisCount > 0) {
+                adddingParenthesis(parentheses, str + ")", openParenthesisCount, closeParenthesisCount-1);
+            }
+        }
+    }
+};
 
 int main(int argc, const char * argv[]) {
+    Solution s;
+    int n = 3;
+    vector<string> parentheses = s.generateParenthesis(n);
     
+    for (auto i : parentheses) {
+        cout << i << " ";
+    }
     
     return 0;
 }
