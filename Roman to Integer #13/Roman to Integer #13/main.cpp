4fa7c35d029893ff6e20faaabccb5d2dbfc98908
Nientai Ho <hntddt123@gmail.com>
2018-08-10 17:03:05 -0500

    Start #13

---
+++ Roman to Integer #13/Roman to Integer #13/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  Roman to Integer #13
+//
+//  Created by Nientai Ho on 2018/8/10.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int romanToInt = 0;
+        
+        return romanToInt;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution s;
+    string result = "9";
+    cout << s.romanToInt(result) << endl;
+    
+    return 0;
+}
