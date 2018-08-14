2f381ad8267e85c32749b8bd24bca9d0aef5b087
Nientai Ho <hntddt123@gmail.com>
2018-08-13 20:28:13 -0500

    start #169

---
+++ Majority Element #169/Majority Element #169/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Majority Element #169
+//
+//  Created by Nientai Ho on 2018/8/13.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+//  The majority element is the element that appears more than ⌊ n/2 ⌋ times in size n array
+//  Input: [2,2,1,1,1,2,2]
+//  Output: 2
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
