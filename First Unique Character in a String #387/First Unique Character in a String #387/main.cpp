1635bde1e47f246c2fb8daf47b2052c12fe6b11b
Nientai Ho <hntddt123@gmail.com>
2018-08-17 14:14:06 -0500

    Start #387

---
+++ First Unique Character in a String #387/First Unique Character in a String #387/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  First Unique Character in a String #387
+//
+//  Created by Nientai Ho on 2018/8/17.
+//  Copyright © 2018年 HNTStudio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
