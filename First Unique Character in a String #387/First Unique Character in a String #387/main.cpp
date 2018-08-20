e0027c76d21403ed1e1378cb713744020f50fc4d
Nientai Ho <hntddt123@gmail.com>
2018-08-20 10:50:39 -0500

    Done #387

--- First Unique Character in a String #387/First Unique Character in a String #387/main.cpp
+++ First Unique Character in a String #387/First Unique Character in a String #387/main.cpp
@@ -5,11 +5,41 @@
 //  Created by Nientai Ho on 2018/8/17.
 //  Copyright © 2018年 HNTStudio. All rights reserved.
 //
+//  Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+//  s = "leetcode"
+//  return 0
+//
+//  s = "loveleetcode",
+//  return 2
+
 
 #include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        if (s.empty()) {
+            return -1;
+        }
+        int alphabets[26] = {0};
+        //store alphabet count
+        for (int i = 0; i < s.length(); i++) {
+            alphabets[s[i] - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (alphabets[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Solution s;
+    string str = "loveleetcode";
+    cout << s.firstUniqChar(str) << endl;
+    
     return 0;
 }
